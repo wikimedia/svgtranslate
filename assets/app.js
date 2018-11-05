3bcedb0a31ff7a723c19a691fe8b7625a989163c
Sam Wilson <sam@samwilson.id.au>
2018-11-05 11:03:31 +0800

    Add jquery.i18n and jquery.uls along with basic selector link
    This sets up frontend i18n message loading, and adds a
    user-menu language settings link that opens the ULS.
    
    Bug: https://phabricator.wikimedia.org/T204849

--- assets/app.js
+++ assets/app.js
@@ -5,11 +5,39 @@ require( 'oojs-ui/dist/themes/wikimediaui/images/icons/language.svg' );
 require( 'oojs-ui/dist/themes/wikimediaui/images/icons/logo-Wikimedia-Commons.svg' );
 require( 'oojs-ui/dist/themes/wikimediaui/images/icons/download.svg' );
 
-// Hide tutorial box (and keep it hidden).
-$( '.close' ).on( 'click', function () {
-	$( '.tutorial' ).slideUp();
-	window.localStorage.setItem( 'tutorial', 'no' );
+// Set up App namespace.
+global.App = {};
+
+// Load i18n message files.
+$( function () {
+	var lang = $( 'html' ).attr( 'lang' ),
+		messagesToLoadUls = {},
+		messagesToLoadApp = {};
+	messagesToLoadUls[ lang ] = appConfig.assetsPath + '/i18n/jquery.uls/' + lang + '.json';
+	messagesToLoadApp[ lang ] = appConfig.assetsPath + '/i18n/app/' + lang + '.json';
+	if ( lang !== 'en' ) {
+		// Also load English files for fallback.
+		messagesToLoadUls.en = appConfig.assetsPath + '/i18n/jquery.uls/en.json';
+		messagesToLoadApp.en = appConfig.assetsPath + '/i18n/app/en.json';
+	}
+	$.i18n().locale = lang;
+	$.i18n().load( messagesToLoadUls );
+	$.i18n().load( messagesToLoadApp )
+		.then( App.addLanguageSettingsLink );
 } );
-if ( window.localStorage.getItem( 'tutorial' ) === 'no' ) {
-	$( '.tutorial' ).hide();
-}
+
+/**
+ * Add the language-settings link to the user nav list. Called after i18n messages are loaded.
+ */
+App.addLanguageSettingsLink = function () {
+	// Create the link.
+	var $langSelectorButton = $( '<a>' )
+		.html( $.i18n( 'language-settings' ) )
+		.attr( 'href', '#lang-dialog' );
+	// Configure ULS on the link.
+	$langSelectorButton.uls( {
+		languages: appConfig.languages
+	} );
+	// Add the link to the DOM.
+	$( 'nav.user ul' ).prepend( $( '<li>' ).append( $langSelectorButton ) );
+};
