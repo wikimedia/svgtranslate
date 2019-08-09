a2914ad23be7834400672c213a593273781e1c19
Sam Wilson <sam@samwilson.id.au>
2019-08-09 10:54:01 +0800

    Add banana checker for i18n files
    Bug: T229991

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = function Gruntfile( grunt ) {
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		banana: {
+			all: {
+				src: 'i18n/',
+				options: {
+					requireMetadata: false
+				}
+			}
+		}
+	} );
+
+};
