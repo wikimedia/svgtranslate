d95327f5e995af958ea77eedd08cf5def6ca7881
TheresNoTime <sam@theresnotime.co.uk>
2022-05-27 01:00:13 +0100

    install npm deps

--- .docker/web/run.sh
+++ .docker/web/run.sh
@@ -7,6 +7,7 @@ chown -R www-data:www-data var .composer vendor
 # Install dependencies.
 chsh -s /bin/bash www-data
 su www-data -c "composer install"
+su www-data -c "npm install"
 
 # Start the Lighttpd web server.
 echo SVG Translate is now at http://localhost:8042
