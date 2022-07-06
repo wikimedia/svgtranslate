a64561366af66ef97720a00357c7bce5c701b096
Sam Wilson <sam@samwilson.id.au>
2022-07-06 12:55:33 +0800

    WIP

--- .docker/web/run.sh
+++ .docker/web/run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Give the web server access to write cache and log files.
-#mkdir -p .composer vendor
 chown -R www-data:www-data var
 
 # Install dependencies.
-#chsh -s /bin/bash www-data
-#su www-data -c "composer install"
-which composer
 composer install
 
 # Start the Lighttpd web server.
