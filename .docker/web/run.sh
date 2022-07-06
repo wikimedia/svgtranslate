db14d1c171e1b2214abc08d873dc62369c360f69
Sam Wilson <sam@samwilson.id.au>
2022-07-06 14:47:30 +0800

    how about now? :-P

--- .docker/web/run.sh
+++ .docker/web/run.sh
@@ -3,9 +3,6 @@
 # Give the web server access to write cache and log files.
 chown -R www-data:www-data var
 
-# Install dependencies.
-composer install -vvv
-
 # Start the Lighttpd web server.
 echo SVG Translate is now at http://localhost:8042
 lighttpd -D -f /etc/lighttpd/lighttpd.conf || exit 1;
